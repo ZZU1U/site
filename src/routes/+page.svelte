eb5694d2b0cdfa298f1b566454b6e51c3207fe54
zzu1u <gl.anohin@yandex.ru>
2025-05-25 23:10:29 +0300

    Messing with tailwind (im newbie in it)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	const timeZone = 'Europe/Moscow';
+
+	let time = $state(new Date());
+
+	let timeFormated = $derived.by(() => {
+		const formatter = new Intl.DateTimeFormat('en-US', {
+			timeStyle: 'long',
+			timeZone
+		});
+		return formatter.format(time);
+	});
+
+	onMount(() => {
+		const interval = setInterval(() => {
+			time = new Date();
+		}, 1000);
+
+		return () => clearInterval(interval);
+	});
+</script>
+
+<div>
+	<img src="/images/me.jpg" alt="my pic" />
+	<h1 class="font-bold underline">Hi, I'm Gleb</h1>
+</div>
+
+<p>
+	I'm currently living in Sochi, Russia. My time is about {timeFormated}
+</p>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+//import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [sveltekit()]
+  plugins: [
+    tailwindcss(),
+    //enhancedImages(),
+    sveltekit(),
+  ],
+  server: {
+    watch: {
+      ignored: ['**/node_modules/**', '**/.svelte-kit/**', '**/dist/**'],
+    },
+  },
 });
